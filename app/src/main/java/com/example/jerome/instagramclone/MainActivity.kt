e3b6340f3ff65043289dccbce72a6a4370350c03
jeromewang <jeromewangcn@outlook.com>
2018-11-13 22:34:29 +1300

    test query on the parse server

--- app/src/main/java/com/example/jerome/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/jerome/instagramclone/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.jerome.instagramclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.FindCallback
+import com.parse.ParseObject
+import com.parse.ParseQuery
+import com.parse.SaveCallback
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var query = ParseQuery.getQuery<ParseObject>("Score")
+        query.whereEqualTo("username","jian")
+        query.findInBackground(FindCallback { objects, e ->
+            if ( objects == null ){
+                Log.i("score","Failed")
+            }else{
+                Log.i("score",objects[0].getString("username"))
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/jerome/instagramclone/StartParseServer.java
@@ -0,0 +1,17 @@
+package com.example.jerome.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class StartParseServer extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+        .applicationId("1b261e2dd81eb79da7be889cb20db0ab95f2123b")
+        .clientKey("18219babed118fdb96255d30e90effb2af751f7c")
+        .server("http://13.236.186.150:80/parse/")
+        .build());
+    }
+}
