498b5d2bc1d78efc00b13fe3f19fbc1e332369c1
jeromewang <jeromewangcn@outlook.com>
2018-11-14 14:02:35 +1300

    finish sign up and log in

--- app/src/main/java/com/example/jerome/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/jerome/instagramclone/MainActivity.kt
@@ -3,25 +3,73 @@ package com.example.jerome.instagramclone
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.parse.FindCallback
-import com.parse.ParseObject
-import com.parse.ParseQuery
-import com.parse.SaveCallback
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.parse.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var signUpstatus : Boolean = true
+    override fun onClick(v: View) {
+
+        if (v.getId() == R.id.changemode){
+            var signupButton : Button = findViewById(R.id.signup) as Button
+            var c : TextView = findViewById(R.id.changemode) as TextView
+            if (signUpstatus){
+                signUpstatus = false
+                signupButton.setText("Log in")
+                c.setText("or Sign up")
+            }else{
+                signUpstatus = true
+                signupButton.setText("Sign up")
+                c.setText("or Log in")
+            }
+        }
+    }
+
+    fun signUp (view : View){
+        var username : EditText = findViewById(R.id.username) as EditText
+        var password : EditText = findViewById(R.id.password) as EditText
+        if(username.getText().toString() == "" || password.getText().toString() == ""){
+            Toast.makeText(this,"A username and password is required",Toast.LENGTH_SHORT).show()
+        }else{
+            if ( signUpstatus){
+                var user : ParseUser = ParseUser()
+                user.setUsername(username.getText().toString())
+                user.setPassword(password.getText().toString())
+                user.signUpInBackground(SignUpCallback {
+                    if ( it == null){
+                        Toast.makeText(this,"Sign up successful",Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this,it.message,Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }else {
+                ParseUser.logInInBackground(username.getText().toString(),password.getText().toString(),
+                        LogInCallback { user, e ->
+                            if ( e == null ){
+                                Toast.makeText(this,"Log in successful",Toast.LENGTH_SHORT).show()
+                            }else{
+                                Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+                            }
+                        })
+            }
+
+        }
+
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var changeText : TextView = findViewById(R.id.changemode) as TextView
+        changeText.setOnClickListener(this)
 
-        var query = ParseQuery.getQuery<ParseObject>("Score")
-        query.whereEqualTo("username","jian")
-        query.findInBackground(FindCallback { objects, e ->
-            if ( objects == null ){
-                Log.i("score","Failed")
-            }else{
-                Log.i("score",objects[0].getString("username"))
-            }
-        })
     }
+
+
 }
