2b398db965df775939661cae85501308685e9d11
jeromewang <jerome.wang1024@gmail.com>
2018-11-15 23:08:07 +1300

    rebuild file

--- app/src/main/java/com/example/jerome/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/jerome/instagramclone/MainActivity.kt
@@ -1,77 +1,85 @@
 package com.example.jerome.instagramclone
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import com.parse.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity() {
 
     var signUpstatus : Boolean = true
-    override fun onClick(v: View) {
 
-        if (v.getId() == R.id.changemode){
-            var signupButton : Button = findViewById(R.id.signup) as Button
-            var c : TextView = findViewById(R.id.changemode) as TextView
-            if (signUpstatus){
-                signUpstatus = false
-                signupButton.setText("Log in")
-                c.setText("or Sign up")
-            }else{
-                signUpstatus = true
-                signupButton.setText("Sign up")
-                c.setText("or Log in")
-            }
-        }
-    }
-
-    fun signUp (view : View){
-        var username : EditText = findViewById(R.id.username) as EditText
-        var password : EditText = findViewById(R.id.password) as EditText
-        if(username.getText().toString() == "" || password.getText().toString() == ""){
-            Toast.makeText(this,"A username and password is required",Toast.LENGTH_SHORT).show()
-        }else{
-            if ( signUpstatus){
-                var user : ParseUser = ParseUser()
-                user.setUsername(username.getText().toString())
-                user.setPassword(password.getText().toString())
-                user.signUpInBackground(SignUpCallback {
-                    if ( it == null){
-                        Toast.makeText(this,"Sign up successful",Toast.LENGTH_SHORT).show()
-                        Log.i("great","great")
-                    }else{
-                        Toast.makeText(this,it.message,Toast.LENGTH_SHORT).show()
-                    }
-                })
-            }else {
-                ParseUser.logInInBackground(username.getText().toString(),password.getText().toString(),
-                        LogInCallback { user, e ->
-                            if ( e == null ){
-                                Toast.makeText(this,"Log in successful",Toast.LENGTH_SHORT).show()
-                            }else{
-                                Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
-                            }
-                        })
-            }
-
-        }
-
-
+    fun hideKeyboard(){
+        var input : InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        input.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var changeText : TextView = findViewById(R.id.changemode) as TextView
-        changeText.setOnClickListener(this)
 
+        var background : RelativeLayout = findViewById(R.id.back) as RelativeLayout
+        var imageView : ImageView = findViewById(R.id.logo) as ImageView
+        var username : EditText = findViewById(R.id.username) as EditText
+        var password : EditText = findViewById(R.id.password) as EditText
+        var button : Button = findViewById(R.id.signup) as Button
+        var chagemode : TextView = findViewById(R.id.changemode) as TextView
+
+        background.setOnClickListener { l : View ->
+            hideKeyboard()
+        }
+
+        imageView.setOnClickListener { l : View ->
+            hideKeyboard()
+        }
+
+        button.setOnClickListener { l : View ->
+            if(username.getText().toString() == "" || password.getText().toString() == ""){
+                Toast.makeText(this,"A username and password is required",Toast.LENGTH_SHORT).show()
+            }else{
+                if ( signUpstatus){
+                    var user : ParseUser = ParseUser()
+                    user.setUsername(username.getText().toString())
+                    user.setPassword(password.getText().toString())
+                    user.signUpInBackground(SignUpCallback {
+                        if ( it == null){
+                            Toast.makeText(this,"Sign up successful",Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this,it.message,Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }else {
+                    ParseUser.logInInBackground(username.getText().toString(),password.getText().toString(),
+                            LogInCallback { user, e ->
+                                if ( e == null ){
+                                    Toast.makeText(this,"Log in successful",Toast.LENGTH_SHORT).show()
+                                }else{
+                                    Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+                                }
+                            })
+                }
+
+            }
+        }
+
+        changemode.setOnClickListener { l : View ->
+            if ( signUpstatus){
+                signUpstatus = false
+                button.setText("Log in")
+                chagemode.setText("Sign up")
+            }else{
+                signUpstatus = true
+                button.setText("Sign up")
+                chagemode.setText("Log in")
+            }
+        }
     }
-
-
 }
+
+
