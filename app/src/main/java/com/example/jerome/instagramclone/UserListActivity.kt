7e9cb767e2ea57f87cd714bb7612694610e6cc49
jeromewang <jerome.wang1024@gmail.com>
2018-11-22 23:06:25 +1300

    need add permission check and store on server

--- app/src/main/java/com/example/jerome/instagramclone/UserListActivity.kt
+++ app/src/main/java/com/example/jerome/instagramclone/UserListActivity.kt
@@ -1,14 +1,20 @@
 package com.example.jerome.instagramclone
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.Toast
 import com.parse.FindCallback
@@ -16,9 +22,18 @@ import com.parse.Parse
 import com.parse.ParseObject
 import com.parse.ParseQuery
 import com.parse.ParseUser
+import kotlinx.android.synthetic.main.activity_user_list.*
 import java.util.jar.Manifest
 
 class UserListActivity : AppCompatActivity() {
+
+    var per = "Manifest.permission.READ_EXTERNAL_STORAGE"
+
+    fun sharePhoto(){
+        var intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.setType("image/*")
+        startActivityForResult(intent,1)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         var menuInflater = getMenuInflater()
@@ -27,39 +42,28 @@ class UserListActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if ( item.itemId == R.id.share ){
-            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if (checkSelfPermission(android.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+        if ( item.itemId == R.id.share){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
 
-                    }
             }else{
-
+                sharePhoto()
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null){
+            var image : Uri = data.data
+            var bitmap : Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,image)
+            var imageView : ImageView = findViewById(R.id.image) as ImageView
+            imageView.setImageBitmap(bitmap)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_list)
-
-        var userlist = findViewById<View>(R.id.userListView) as ListView
-        var arrayList  = ArrayList<String>()
-
-        var query : ParseQuery<ParseUser> = ParseUser.getQuery()
-        query.whereNotEqualTo("username",ParseUser.getCurrentUser().username)
-        query.findInBackground { objects, e ->
-            if (e == null && objects.size > 0){
-                for ( user in objects){
-                    arrayList.add(user.getString("username").toString())
-                }
-            }else{
-                Log.i("ttt","no")
-            }
-        }
-
-        var adapter : ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList)
-        userlist.setAdapter(adapter)
-
     }
 }
